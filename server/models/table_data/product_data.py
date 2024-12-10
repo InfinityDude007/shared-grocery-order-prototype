bc757fd2d4331dcac796b995c58b08ee47c69e03
Armaan <armaanj32@gmail.com>
2024-12-10 14:33:18 +0400

    Add more hardcoded data to product_data.py for testing on Postman

--- server/models/table_data/product_data.py
+++ server/models/table_data/product_data.py
@@ -9,5 +9,15 @@ hardcoded_products = [
     {'product_id': '1007', 'product_name': 'Soda', 'product_price': 2.5, 'product_stock': 200},
     {'product_id': '1008', 'product_name': 'Juice', 'product_price': 1.5, 'product_stock': 100},
     {'product_id': '1009', 'product_name': 'Eggs', 'product_price': 15.0, 'product_stock': 130},
-    {'product_id': '1010', 'product_name': 'Laban', 'product_price': 1.0, 'product_stock': 210}
+    {'product_id': '1010', 'product_name': 'Laban', 'product_price': 1.0, 'product_stock': 210},
+    {'product_id': '1011', 'product_name': 'Tomato', 'product_price': 3.0, 'product_stock': 95},
+    {'product_id': '1012', 'product_name': 'Potato', 'product_price': 2.2, 'product_stock': 150},
+    {'product_id': '1013', 'product_name': 'Onion', 'product_price': 1.8, 'product_stock': 140},
+    {'product_id': '1014', 'product_name': 'Carrot', 'product_price': 2.5, 'product_stock': 110},
+    {'product_id': '1015', 'product_name': 'Lettuce', 'product_price': 1.3, 'product_stock': 160},
+    {'product_id': '1016', 'product_name': 'Chicken Breast', 'product_price': 7.5, 'product_stock': 70},
+    {'product_id': '1017', 'product_name': 'Ground Beef', 'product_price': 6.0, 'product_stock': 90},
+    {'product_id': '1018', 'product_name': 'Cheese', 'product_price': 4.5, 'product_stock': 80},
+    {'product_id': '1019', 'product_name': 'Yogurt', 'product_price': 3.2, 'product_stock': 110},
+    {'product_id': '1020', 'product_name': 'Cereal', 'product_price': 3.8, 'product_stock': 120}
 ]
