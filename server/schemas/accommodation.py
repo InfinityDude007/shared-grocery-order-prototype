bb8a201ca64fc807a290a2cdbca7e767a0a73cef
Glitchcoding <sathvikkonduparti@gmail.com>
2024-12-12 15:35:09 +0400

    Add schemas for accommodation model

---
+++ server/schemas/accommodation.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import List, Literal
+
+
+# Schema for the details related to an individual accommodation
+class AccommodationData(BaseModel):
+    accommodation_id: str
+    accommodation_users: List[str]
+
+
+# Schema for the list of all accommodation details available
+class AllAccommodationsResponse(BaseModel):
+    accommodations: List[AccommodationData]
+
+
+# Schema for showing the outcome of a put, post, or delete request
+class AccommodationsSuccessResponse(BaseModel):
+    action: Literal['Update Accommodation Data', 'Add New Accommodation Data', 'Delete Accommodation Data']
+    success: bool
+    message: str
