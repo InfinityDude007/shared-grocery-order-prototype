3221d0b3ef7a1f2c51ca07cc856d44785978af38
Glitchcoding <sathvikkonduparti@gmail.com>
2024-12-12 15:21:18 +0400

    Update init with created data array

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -3,6 +3,7 @@ from .base_model import BaseModel
 from .supermarket_products import SupermarketProducts
 from .users import Users
 from .orders import Orders
+from .accommodation import Accommodation
 
 # hardcoded data
 from .table_data import hardcoded_products, hardcoded_users, hardcoded_orders , hardcoded_accommodations
---
+++ server/models/table_data/accommodation_data.py
@@ -0,0 +1,24 @@
+# hardcoded data for the Accommodations table for prototyping purposes
+
+hardcoded_accommodations = [
+    {
+        'accom_id': 'A001',
+        'accommodation_users': ['U001', 'U006', 'U011', 'U016']
+    },
+    {
+        'accom_id': 'A002',
+        'accommodation_users': ['U002', 'U007', 'U012', 'U017']
+    },
+    {
+        'accom_id': 'A003',
+        'accommodation_users': ['U003', 'U008', 'U013', 'U018']
+    },
+    {
+        'accom_id': 'A004',
+        'accommodation_users': ['U004', 'U009', 'U014', 'U019']
+    },
+    {
+        'accom_id': 'A005',
+        'accommodation_users': ['U005', 'U010', 'U015', 'U020']
+    }
+]
