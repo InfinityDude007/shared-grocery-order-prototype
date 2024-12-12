8e9e2b8c4ccd6b9678137c39ae8e46ad1cda40fd
Armaan <armaanj32@gmail.com>
2024-12-12 18:18:27 +0400

    Fix typo in accommodation_router import

--- server/api/routers/__init__.py
+++ server/api/routers/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from .users_routers import router as users_router
 from .orders_routers import router as orders_router
-from .accommodation_routers import router as accommodation_routers
+from .accommodation_routers import router as accommodation_router
 
 router = APIRouter()
 router.include_router(users_router, tags="Users")
 router.include_router(orders_router, tags="Orders")
-router.include_router(accommodation_routers, tags="Accommodation")  
+router.include_router(accommodation_router, tags="Accommodation")  
