186a63da45ea6152908b80eb50bda541a3f9e78f
Armaan <armaanj32@gmail.com>
2024-12-12 17:16:29 +0400

    Add cost splitting routers to __init__.py and main router

--- server/api/routers/__init__.py
+++ server/api/routers/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from .users_routers import router as users_router
 from .orders_routers import router as orders_router
+from .cost_splitting_routers import router as cost_splitting_router
 
 router = APIRouter()
 router.include_router(users_router, tags="Users")
